438d2a3a269249ff5b70374a968381f34daa35cc
little-meow-meow <204685920+little-meow-meow@users.noreply.github.com>
2025-07-01 21:00:48 -0700

    fix source comments
    Signed-off-by: little-meow-meow <204685920+little-meow-meow@users.noreply.github.com>

--- Content.Client/Paper/UI/StampWidget.xaml.cs
+++ Content.Client/Paper/UI/StampWidget.xaml.cs
@@ -12,7 +12,7 @@ namespace Content.Client.Paper.UI;
 [GenerateTypedNameReferences]
 public sealed partial class StampWidget : PanelContainer
 {
-    private StyleBoxTexture? _borderTexture;
+    private StyleBoxTexture? _borderTexture;  // imp: made nullable
     private ShaderInstance? _stampShader;
 
     public float Orientation
@@ -23,6 +23,10 @@ public sealed partial class StampWidget : PanelContainer
 
     public StampDisplayInfo StampInfo {
         set {
+            // begin imp
+            // StampedByLabel.Text = Loc.GetString(value.StampedName);
+            // StampedByLabel.FontColorOverride = value.StampedColor;
+            // ModulateSelfOverride = value.StampedColor;
             var icon = value.StampLargeIcon;
             if (icon != null)
             {
@@ -45,24 +49,34 @@ public sealed partial class StampWidget : PanelContainer
                 StampedByLabel.FontColorOverride = value.StampedColor;
                 ModulateSelfOverride = value.StampedColor;
             }
+            // end imp
         }
     }
 
     public StampWidget()
     {
         RobustXamlLoader.Load(this);
+        // begin imp
         var prototypes = IoCManager.Resolve<IPrototypeManager>();
         _stampShader = prototypes.Index<ShaderPrototype>("PaperStamp").InstanceUnique();
 
         if (PanelOverride != null)
             return;
+        // end imp
 
         var resCache = IoCManager.Resolve<IResourceCache>();
         var borderImage = resCache.GetResource<TextureResource>(
                 "/Textures/Interface/Paper/paper_stamp_border.svg.96dpi.png");
-        _borderTexture = new StyleBoxTexture { Texture = borderImage };
+        _borderTexture = new StyleBoxTexture {
+            Texture = borderImage,
+        };
         _borderTexture.SetPatchMargin(StyleBoxTexture.Margin.All, 7.0f);
         PanelOverride = _borderTexture;
+
+        // begin imp
+        // var prototypes = IoCManager.Resolve<IPrototypeManager>();
+        // _stampShader = prototypes.Index<ShaderPrototype>("PaperStamp").InstanceUnique();
+        // end imp
     }
 
     protected override void Draw(DrawingHandleScreen handle)
