5602864b318b6514d3f27fe65420b8bf719bd2a1
little-meow-meow <204685920+little-meow-meow@users.noreply.github.com>
2025-08-27 21:10:38 -0700

    fix merge conflict
    Signed-off-by: little-meow-meow <204685920+little-meow-meow@users.noreply.github.com>

--- Content.Shared/Hands/EntitySystems/SharedHandsSystem.cs
+++ Content.Shared/Hands/EntitySystems/SharedHandsSystem.cs
@@ -36,12 +36,7 @@ public abstract partial class SharedHandsSystem
         InitializeDrop();
         InitializePickup();
         InitializeRelay();
-<<<<<<< HEAD
-        InitializeEventListeners();
-=======
-        InitializeEventListeners(); //Starlight
         InitializeOffer(); // Starlight
->>>>>>> 560c65aa435 (Merge pull request #1086 from FoxxoTrystan/offer-item)
 
         SubscribeLocalEvent<HandsComponent, ComponentInit>(OnInit);
         SubscribeLocalEvent<HandsComponent, MapInitEvent>(OnMapInit);
