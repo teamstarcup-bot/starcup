81d40cbd6ea2655703c2196dde965d1faec12b07
PrincessPengy <114524259+princesspengy@users.noreply.github.com>
2025-09-10 21:08:15 -0700

    fix merge conflict resolution

--- Content.Server/Chemistry/EntitySystems/InjectorSystem.cs
+++ Content.Server/Chemistry/EntitySystems/InjectorSystem.cs
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-||||||| parent of 5acbab1b421 (Add chemical skimmer (#3496))
-using Content.Server.Body.Components;
-=======
 using System.Linq; // DeltaV
-using Content.Server.Body.Components;
->>>>>>> 5acbab1b421 (Add chemical skimmer (#3496))
 using Content.Server.Body.Systems;
 using Content.Shared.Chemistry;
 using Content.Shared.Chemistry.Components;
@@ -384,13 +378,7 @@ public sealed class InjectorSystem : SharedInjectorSystem
         var removedSolution = SolutionContainers.Draw(target.Owner, targetSolution, realTransferAmount);
 
         // Add back non-whitelisted reagents to the target solution
-<<<<<<< HEAD
         SolutionContainers.TryAddSolution(targetSolution, temporarilyRemovedSolution);
-||||||| parent of 5acbab1b421 (Add chemical skimmer (#3496))
-        applicableTargetSolution.AddSolution(temporarilyRemovedSolution, null);
-=======
-        SolutionContainers.TryAddSolution(targetSolution, temporarilyRemovedSolution); // DeltaV - fix bug with visualization for skimmers
->>>>>>> 5acbab1b421 (Add chemical skimmer (#3496))
 
         if (!SolutionContainers.TryAddSolution(soln.Value, removedSolution))
         {
